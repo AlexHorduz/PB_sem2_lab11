bd3b2877a1df65b486cd3da57374dcbeea7c3a42
AlexHorduz <alex.horduz@gmail.com>
2021-05-03 11:42:05 +0300

    Initial commit

---
+++ src/lab11/Main.java
@@ -0,0 +1,31 @@
+package lab11;
+
+import lab11.interfaces.Calculator;
+import lab11.models.Rectangle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static  java.lang.Math.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calcEquation3 = (a, b, c, d) -> pow(2 * sin(a) + cos(abs(b * sqrt(c))), d);
+        task1(calcEquation3, 1, 2, 3, 4);
+        System.out.println("\n");
+
+
+        Rectangle r1 = new Rectangle(3, 5);
+        Rectangle r2 = new Rectangle(6, 22);
+        Rectangle r3 = new Rectangle(8, 1);
+        Collection<Rectangle> list = new ArrayList<>(Arrays.asList(r1, r2, r3));
+        list.forEach(rect -> System.out.println(rect));
+    }
+
+    public static void task1(Calculator calc, double a, double b, double c, double d) {
+        System.out.println(a + " " + b + " " + c + " " + d);
+        System.out.println(calc.calculate(a, b, c, d));
+    }
+}
---
+++ src/lab11/interfaces/Calculator.java
@@ -0,0 +1,6 @@
+package lab11.interfaces;
+
+@FunctionalInterface
+public interface Calculator {
+    double calculate(double a, double b, double c, double d);
+}
